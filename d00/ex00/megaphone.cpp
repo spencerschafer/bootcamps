e7f8dcd659ca067c518e5f80424cc74c230d9390
spencerschafer <spencerschafer@gmail.com>
2018-03-31 20:52:00 +0200

    added d00 ex00

---
+++ d00/ex00/megaphone.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+/* 
+ * In the function toUpper, you can do either:
+ * 		*str = (*str - 32);
+ * 	or 
+ * 		(char) *str - 32;
+ *
+ * 	The key take away here is that without the assignment, it prints an 
+ * 	integer, and not a character, as the programme is doing subtraction on
+ * 	two integers. If you want to omit assigning a new value then a cast is
+ * 	necessary in order to print a character.
+ *
+ * 	Completed on 31/03/2018 6:25pm
+ */
+
+void toUpper(char *str)
+{
+	while (*str)
+	{
+		if (*str >= 'a' && *str <= 'z')
+			std::cout << (char) (*str - 32);
+		else 
+			std::cout << *str;
+		str++;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int index;
+	char *str;
+
+	index = 1;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (index < argc)
+	{
+		str = argv[index];
+		toUpper(argv[index]);
+		index++;
+	}
+	std::cout << std::endl;
+	return 0;
+}
