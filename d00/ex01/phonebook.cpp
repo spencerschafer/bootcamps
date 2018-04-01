271198a5a74fd95614abb678e2c94dccde6b0792
spencerschafer <spencerschafer@gmail.com>
2018-04-02 01:43:54 +0200

    Refactor phonebook.cpp and add file phonebook.hpp

--- d00/ex01/phonebook.cpp
+++ d00/ex01/phonebook.cpp
@@ -1,161 +1,70 @@
 #include <iostream>
+#include "phonebook.hpp"
 //std::cout << std::endl;
-class Contact
+
+void printContact(Contact contact)
 {
+	std::cout << "First Name: " << contact.firstName << std::endl;
+	std::cout << "Last Name: "<< contact.lastName << std::endl;
+	std::cout << "Nickname: "<< contact.nickname << std::endl;
+	std::cout << "Login: " << contact.login << std::endl;
+	std::cout << "Login: " << contact.login << std::endl;
+	std::cout << "Email: " << contact.emailAddress << std::endl;
+	std::cout << "Phone Number: " << contact.phoneNumber << std::endl;
+	std::cout << "Birthday: " << contact.birthday << std::endl;
+	std::cout << "Favourite Meal: " << contact.favouriteMeal << std::endl;
+	std::cout << "Underwear Colour" << contact.underwearColour << std::endl;
+	std::cout << "Darkest Secret: " << contact.darkestSecret << std::endl;
+	std::cout << std::endl;
+}
 
-	public:
-		std::string firstName;
-		std::string lastName;
-		std::string nickname;
-		std::string login;
-		std::string postalAddress;
-		std::string emailAddress;
-		std::string phoneNumber;
-		std::string birthday;
-		std::string favouriteMeal;
-		std::string underwearColour;
-		std::string darkestSecret;
-
-		void setFirstName (std::string firstName)
-		{
-			this->firstName = firstName;
-		}
-
-		void setLastName (std::string lastName)
-		{
-			this->lastName = lastName;
-		}
-
-		void setNickname (std::string nickname)
-		{
-			this->nickname = nickname;
-		}
-
-		void setLogin (std::string login)
-		{
-			this->login = login;
-		}
-
-		void setPostalAddress (std::string postalAddress)
-		{
-			this->postalAddress = postalAddress;
-		}
-
-		void setEmailAddress (std::string emailAddress)
-		{
-			this->emailAddress = emailAddress;
-		}
-
-		void setPhoneNumber (std::string phoneNumber)
-		{
-			this->phoneNumber = phoneNumber;
-		}
-
-		void setBirthday (std::string birthday)
-		{
-			this->birthday = birthday;
-		}
-
-		void setFavouriteMeal (std::string favouriteMeal)
-		{
-			this->favouriteMeal = favouriteMeal;
-		}
-
-		void setUnderwearColour (std::string underwearColour)
-		{
-			this->underwearColour = underwearColour;
-		}
-
-		void setDarkestSecret (std::string darkestSecret)
-		{
-			this->darkestSecret = darkestSecret;
-		}
-
-		void printLogin()
-		{
-			std::cout << "Login: " << this->login << std::endl;
-		}
-
-		void printContact()
-		{
-			std::cout << "First Name: " << this->firstName << std::endl;
-
-			std::cout << "Last Name: "<< this->lastName << std::endl;
-
-			std::cout << "Nickname: "<< this->nickname << std::endl;
-
-			std::cout << "Login: " << this->login << std::endl;
-
-			std::cout << "Login: " << this->login << std::endl;
-
-			std::cout << "Email: " << this->emailAddress << std::endl;
-
-			std::cout << "Phone Number: " << this->phoneNumber << std::endl;
-
-			std::cout << "Birthday: " << this->birthday << std::endl;
-
-			std::cout << "Favourite Meal: " << this->favouriteMeal << std::endl;
-
-			std::cout << "Underwear Colour" << this->underwearColour << std::endl;
-
-			std::cout << "Darkest Secret: " << this->darkestSecret << std::endl;
-		}
-};
-
-void promptForm(Contact contact)
+void promptForm(Contact *contact)
 {
-	std::string input << std::endl;
+	std::string input;
+	std::cout << std::endl;
 	std::cout << "Pleaese fill in the following contact details:" << std::endl;
 
 	std::cout << "First Name: ";
-	std::cin >> input; 
-	contact.setFirstName(input);
+	std::cin >> contact->firstName;
 
 	std::cout << "Last Name: ";
-	std::cin >> input; 
-	contact.setLastName(input);
+	std::cin >> contact->lastName;
 
 	std::cout << "Nickname: ";
-	std::cin >> input; 
-	contact.setNickname(input);
+	std::cin >> contact->nickname;
 
 	std::cout << "Login: ";
-	std::cin >> input; 
-	contact.setLogin(input);
+	std::cin >> contact->login;
 
 	std::cout << "Postal Address: ";
-	std::cin >> input; 
-	contact.setPostalAddress(input);
+	std::cin >> contact->postalAddress;
 
 	std::cout << "Email: ";
-	std::cin >> input; 
-	contact.setEmailAddress(input);
+	std::cin >> contact->emailAddress;
 
 	std::cout << "Phone Number: ";
-	std::cin >> input; 
-	contact.setPhoneNumber(input);
+	std::cin >> contact->phoneNumber;
 
 	std::cout << "Birthday: ";
-	std::cin >> input; 
-	contact.setBirthday(input);
+	std::cin >> contact->birthday;
 
 	std::cout << "Favourite Meal: ";
-	std::cin >> input; 
-	contact.setFavouriteMeal(input);
+	std::cin >> contact->favouriteMeal;
 
 	std::cout << "Underwear Colour: ";
-	std::cin >> input;
-	contact.setUnderwearColour(input);
+	std::cin >> contact->underwearColour;
 
 	std::cout << "Darkest Secret: ";
-	std::cin >> input; 
-	contact.setDarkestSecret(input);
+	std::cin >> contact->darkestSecret;
+	std::cout << std::endl << std::endl;
+
 }
 
 int main(void)
 {
 
-	Contact contact[8];
+	int SIZE = 8;
+	Contact contact[SIZE];
 	int count = 0;
 
 	while (true)
@@ -173,57 +82,50 @@ int main(void)
 
 		if (input == "ADD" || input == "A")
 		{
-			++count;
-			if (count > 8)
+			if (count >= SIZE)
 			{
+				std::cout << std::endl;
 				std::cout << "Your phonebook is currently full.";
+				std::cout << std::endl << std::endl;
 			}
 			else
-			{
-				promptForm(contact[count]);
-				std::cout << std::endl;
-
-				std::cout << contact[count].firstName << std::endl;
-			}
+				promptForm(&contact[count]);
+			++count;
 		}
 		else if (input == "SEARCH" || input == "S")
 		{
 			int index = 0;
-			bool found = false;
 			std::string input;
 
-			std::cout << std::endl << "Enter Login details: " << std::endl;
-
+			std::cout << std::endl;
+			std::cout << "Enter index: " << std::endl;
 			std::cin >> input;
-			std::cout << "Input: [" + input + "]" << std::endl;
-			while (index < 8)
+			std::cout << std::endl;
+
+			while (index < count)
 			{
-				std::cout << (contact[index]).login;
-				if (contact[index].login.compare(input))
+				if (!contact[index].login.compare(input))
 				{
-					found = true;
+					printContact(contact[index]);
 					break;
 				}
-				++index;
+				else
+					++index;
 			}
-			printf("FOUND: %d\n", found);
-			if (found) {
-				contact[index].printContact();
-				std::cout << "Login1: [" + (contact[index]).login + "]";
-				std::cout << std::endl << "00";
-				(contact[index]).printLogin();
-			}
-			else
+			if (index == count)
 			{
-				std::cout << std::endl << "That person does not exist.";
-				std::cout << std::endl << std::endl;
+				std::cout << std::endl;
+				std::cout << "No contact exists with the details '" << input;
+				std::cout << "'" << std::endl << std::endl;
 			}
 		}
 		else if (input == "EXIT" || input == "E")
 			return (0);
 		else
 		{
-			std::cout << "Please be more specific with your choice." << std::endl;
+			std::cout << std::endl;
+			std::cout << "Please be more specific with your choice.";
+			std::cout << std::endl << std::endl;
 		}
 	}
 	std::cout << std::endl;
---
+++ d00/ex01/phonebook.hpp
@@ -0,0 +1,25 @@
+#ifndef PHONEBOOK_H
+#define Contact_Class_H
+
+#include <iostream>
+
+class Contact
+{
+	public:
+		std::string firstName;
+		std::string lastName;
+		std::string nickname;
+		std::string login;
+		std::string postalAddress;
+		std::string emailAddress;
+		std::string phoneNumber;
+		std::string birthday;
+		std::string favouriteMeal;
+		std::string underwearColour;
+		std::string darkestSecret;
+};
+
+void printContact(Contact contact);
+void promptForm(Contact *contact);
+
+#endif
