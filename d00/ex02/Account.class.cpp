f2bf545614b50644b53fc3f9d49474d45c5f8f96
spencerschafer <spencerschafer@gmail.com>
2018-04-30 01:34:19 +0200

    Complete functions in Account.class.cpp
    - Modified logs to accurately portray stdout

--- d00/ex02/Account.class.cpp
+++ d00/ex02/Account.class.cpp
@@ -1,89 +1,130 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 #include "Account.class.hpp"
 
-static int	getNbAccounts(Account account)
+
+// Understand why we need these variables here. Basically understand how classes are instantiated in C++
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+int	Account::getNbAccounts(void)
 {
-	//std::cout << _nbAccounts;
 	return _nbAccounts;
 }
 
-static int	getTotalAmount(void)
+int	Account::getTotalAmount(void)
 {
-	//std::cout << _totalAmount;
 	return _totalAmount;
 }
 
-static int	getNbDeposits(void)
+int	Account::getNbDeposits(void)
 {
-	//std::cout << _totalNbDeposits;
 	return _totalNbDeposits;
 }
 
-static int	getNbWithdrawals( void )
+int	Account::getNbWithdrawals( void )
 {
-	//std::cout << _totalNbWithdrawals;	
 	return _totalNbWithdrawals;
 }
 
-static void	displayAccountsInfos( void );
+void	Account::displayAccountsInfos( void )
 {
+	//[20150406_153629] accounts:8;total:20049;deposits:0;withdrawals:0
 	_displayTimestamp();
-	std::cout << 
-}	
-
+	std::cout << "accounts:" << getNbAccounts() << ";total:" << getTotalAmount() << ";";
+	std::cout << "deposits:" << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << ";" << std::endl;
+}
 
 Account::Account(int initial_deposit)
 {
-	// static amounts
+	//[20150406_153629] index:0;amount:42;created
+
+    this->_accountIndex = _nbAccounts;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+
 	_nbAccounts += 1;
 	_totalAmount += initial_deposit;
-	_totalNbDeposits += 1;
 
-	//specifc class amounts
-	Account._accountIndex += 1;
-	Account._amount += initial deposit;
-	Account._nbDeposits += 1;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
 }
 
-~Account(void);
-
-void makeDeposit(int deposit)
+// Understand destructors
+Account::~Account(void)
 {
+	//[20150406_153629] index:7;amount:8942;closed
+    _displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
+};
+
+void	Account::makeDeposit(int deposit)
+{
+	//[20150406_153629] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
 	_totalAmount += deposit;
 	_totalNbDeposits += 1;
 
-	_amount += deposit;
-	_nbDeposits += 1;
+	this->_amount += deposit;
+	this->_nbDeposits += 1;
+
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount - deposit << ";deposit:" << deposit << ";";
+	std::cout << "amount:" <<_amount << ";nb_deposits:" << _nbDeposits << ";" << std::endl;
 }
 
-bool makeWithdrawal(int withdrawal)
+
+//TODO: Fix
+bool Account::makeWithdrawal(int withdrawal)
 {
+//	[20150406_153629] index:0;p_amount:47;withdrawal:refused
+//	[20150406_153629] index:1;p_amount:819;withdrawal:34;amount:785;nb_withdrawals:1
 	_totalAmount -= withdrawal;
 	_totalNbWithdrawals += 1;
 
-	_amount -= withdrawal;
-	_nbWithdrawals += 1;
-	//return proper value
-	return (1);
+	this->_amount -= withdrawal;
+	this->_nbWithdrawals += 1;
+	std::cout << "makeWithdrawal:" << std::endl;
+	return (true);
 }
 
-int checkAmount(void) const
+//  BONUS - implement checkAmount
+int Account::checkAmount(void) const
 {
-	return (Account.amount);
+	std::cout << "checkAmount: " << std::endl;
+    return (0);
 }
 
-void displayStatus(void) const
+void Account::displayStatus(void) const
 {
-	//code
+	//[20150406_153629] index:0;amount:42;deposits:0;withdrawals:0
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";";
+	std::cout << "withdrawals:" << _nbWithdrawals << std::endl;
 }
 
-static void	_displayTimestamp(void)
+void	Account::_displayTimestamp(void)
 {
-	
+	// Find out how the two lines below work exactly
+	time_t now = time(0);
+	tm *ltm = localtime(&now);
+
+	std::cout << "[";
+	std::cout << 1900 + ltm->tm_year;
+	std::cout << std::setw(2) << std::setfill('0') << 1 + ltm->tm_mon;
+	std::cout << std::setw(2) << std::setfill('0') << ltm->tm_mday;
+	std::cout << "_";
+	std::cout << std::setw(2) << std::setfill('0') << ltm->tm_hour;
+	std::cout << std::setw(2) << std::setfill('0') << ltm->tm_min;
+	std::cout << std::setw(2) << std::setfill('0') << ltm->tm_sec;
+    std::cout << "] ";
 }
 
-int main(void)
+//TODO: Understand this constructor
+Account::Account(void)
 {
-    Account account;
-}
+	return;
+};
